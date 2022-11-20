9569145467d95975609d9792b18606c9c675270c
MaiSone <msone@nikkeibp.co.jp>
2022-11-20 20:51:03 +0900

    add:index.html

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  fs.readFile('index.html', 'UTF-8',
+  (error, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 });
 
 server.listen(port, hostname, () => {
